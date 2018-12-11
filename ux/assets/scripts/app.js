228179a7fb2562bb5297b5b5e2ab9159fbd65fda
Chris Fournier <chris.d.fournier@gmail.com>
2018-12-11 15:52:03 -0500

    New flows and css adjustments
    Should be a fairly complete app now with nearly all paths covered.

--- ux/assets/scripts/app.js
+++ ux/assets/scripts/app.js
@@ -1,3 +1,18 @@
+// LOADING SCREEN TIMEOUT
 window.setTimeout(function(){
   $('.loading-screen').addClass('slideOutUp')
 },4000);
+
+
+// LOG IN AND REGISTRATION FORMS
+$('#create-account').click(function() {
+  $('.log-in').addClass('hide');
+  setTimeout(function() {
+    $('.register').addClass('show'); }, 10);
+});
+
+$('#cancel').click(function() {
+  $('.register').removeClass('show');
+  setTimeout(function() {
+    $('.log-in').removeClass('hide'); }, 10);
+});
