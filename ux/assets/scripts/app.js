d0625cfca7dfe196d75cb985036f31ad3b0ffb83
Chris Fournier <chris.d.fournier@gmail.com>
2018-12-12 10:12:48 -0500

    Final pre-hack updates

--- ux/assets/scripts/app.js
+++ ux/assets/scripts/app.js
@@ -8,11 +8,29 @@ window.setTimeout(function(){
 $('#create-account').click(function() {
   $('.log-in').addClass('hide');
   setTimeout(function() {
-    $('.register').addClass('show'); }, 10);
+    $('.register.one').addClass('show'); }, 10);
 });
 
-$('#cancel').click(function() {
-  $('.register').removeClass('show');
+$('#new-account-next').click(function() {
+  $('.register.one').removeClass('show');
+  setTimeout(function() {
+    $('.register.two').addClass('show'); }, 10);
+});
+
+$('#new-account-back').click(function() {
+  $('.register.two').removeClass('show');
+  setTimeout(function() {
+    $('.register.one').addClass('show'); }, 10);
+});
+
+$('#cancel-one').click(function() {
+  $('.register.one').removeClass('show');
+  setTimeout(function() {
+    $('.log-in').removeClass('hide'); }, 10);
+});
+
+$('#cancel-two').click(function() {
+  $('.register.two').removeClass('show');
   setTimeout(function() {
     $('.log-in').removeClass('hide'); }, 10);
 });
